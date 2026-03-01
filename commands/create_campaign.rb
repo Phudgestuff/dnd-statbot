afd8de01bf042c89db77f94449f118cd66fee001
Phudgestuff <bellhugo30@gmail.com>
2026-03-01 18:48:33 +0000

    Add create_campaign command

--- commands/create_campaign.rb
+++ commands/create_campaign.rb
@@ -1,9 +1,26 @@
 require 'discordrb'
+require_relative '../db/json_functions'
 
-module CreateCampaign
+module Campaigns
   extend Discordrb::EventContainer
 
   application_command(:create_campaign) do |event|
-    event.respond(content: "this command doesn't do anything yet...") # TODO make this do something
+    data = DB.get_data
+
+    # notify the user if the campaign has already been created
+    if data.key?(event.channel.id)
+      event.respond(content: "A campaign has already been assigned to this channel", ephemeral: true)
+      return
+    end
+
+    # otherwise set up the campaign entry
+    campaign_name = event.options['name'] || event.channel.name # set the dnd name to the channel name if no name is provided
+
+    data[event.channel.id] = {name: campaign_name}
+    # write changes
+    DB.update data
+
+    # inform user of changes
+    event.respond(content: "Created a DnD campaign called `#{campaign_name}` and assigned it to this channel")
   end
 end
--- main.rb
+++ main.rb
@@ -11,11 +11,16 @@ bot = Discordrb::Bot.new(
   intents: [:direct_messages]
 )
 
-bot.include! CreateCampaign
+bot.include! Campaigns
 
 bot.ready do
+  # /ping
   bot.register_application_command(:ping, 'Check if the bot is alive', server_id: server_id)
-  bot.register_application_command(:create_campaign, 'Create a new DnD campaign to store characters in', server_id: server_id)
+
+  # /create_campaign <campaign name>
+  bot.register_application_command(:create_campaign, 'Create a new DnD campaign to store characters in', server_id: server_id) do |conf|
+    conf.string('name', 'The name of the campaign', required: false)
+  end
 end
 
 bot.application_command(:ping) do |event|
